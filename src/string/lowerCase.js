0642448192708ec5e3cd4c75c03ea612f41240cc
Randson Oliveira <orandson@gmail.com>
2016-07-25 09:55:42 -0300

    feat(lower-case): create lowerCase function/spec

---
+++ src/string/lowerCase.js
@@ -0,0 +1,7 @@
+import toString from 'lang/toString'
+
+export default function lowerCase (str) {
+  str = toString(str)
+
+  return str.toLowerCase()
+}
---
+++ tests/string/lowerCase.spec.js
@@ -0,0 +1,18 @@
+import lowerCase from 'string/lowerCase'
+
+describe('String', () => {
+  describe('#lowerCase', () => {
+    it('should return lowerCase for all upperCase string', () => {
+      expect(lowerCase('FOO')).to.eql('foo')
+      expect(lowerCase('BAR')).to.eql('bar')
+    })
+
+    it('should treat null as empty string', () => {
+      expect(lowerCase(null)).to.eql('')
+    })
+
+    it('should treat undefined as empty string', () => {
+      expect(lowerCase(void 0)).to.eql('')
+    })
+  })
+})
