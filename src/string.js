238060248ac23fb7449aa2bf8bba69820aae7719
Randson Oliveira <orandson@gmail.com>
2016-07-29 10:56:04 -0300

    fix(string): add ltrim to import strings
    Also, remove './' from init import

--- src/string.js
+++ src/string.js
@@ -1,14 +1,16 @@
-import contains from './string/contains'
-import endsWith from './string/endsWith'
-import lowerCase from './string/lowerCase'
-import startsWith from './string/startsWith'
-import typeCast from './string/typeCast'
-import upperCase from './string/upperCase'
+import contains from 'string/contains'
+import endsWith from 'string/endsWith'
+import lowerCase from 'string/lowerCase'
+import ltrim from 'string/ltrim'
+import startsWith from 'string/startsWith'
+import typeCast from 'string/typeCast'
+import upperCase from 'string/upperCase'
 
 export {
   contains,
   endsWith,
   lowerCase,
+  ltrim,
   startsWith,
   typeCast,
   upperCase
