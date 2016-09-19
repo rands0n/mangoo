e475730ba95b6b28bc3484e419213012f130d7f1
Randson Oliveira <orandson@gmail.com>
2016-09-19 18:54:07 -0300

    feat(array): create isEmpty function/spec array

---
+++ src/array/isEmpty.js
@@ -0,0 +1,3 @@
+export const isEmpty = (arr) => {
+  return !(arr && arr.length > 0)
+}
---
+++ tests/array/isEmpty.spec.js
@@ -0,0 +1,35 @@
+import { isEmpty } from 'array'
+
+describe('Array', () => {
+  describe('#isEmpty', () => {
+    context('when passing valid arguments', () => {
+      it('should return true to array', () => {
+        expect(isEmpty([])).to.be.true
+      })
+
+      it('should return true for objects', () => {
+        expect(isEmpty({})).to.be.true
+      })
+
+      context('when value is undefined, null or void', () => {
+        it('should return true for null', () => {
+          expect(isEmpty(null)).to.be.true
+        })
+
+        it('should return true for void', () => {
+          expect(isEmpty(void 0)).to.be.true
+        })
+
+        it('should return true for undefined', () => {
+          expect(isEmpty(undefined)).to.be.true
+        })
+      })
+    })
+
+    context('when passing invalid arguments', () => {
+      it('should return false when the array has values', () => {
+        expect(isEmpty([1, 2])).to.be.false
+      })
+    })
+  })
+})
