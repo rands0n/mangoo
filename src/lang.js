15932b164a40d492aa779fe2fc3114e9bc7b5b1a
Randson Oliveira <orandson@gmail.com>
2016-10-01 22:47:26 -0300

    fix(isObject): map isObject function to lang exports

--- src/lang.js
+++ src/lang.js
@@ -1,9 +1,11 @@
 import { isArray } from './lang/isArray'
 import { isNull } from './lang/isNull'
+import { isObject } from './lang/isObject.js'
 import { toString } from './lang/toString'
 
 export {
   isArray,
   isNull,
+  isObject,
   toString
 }
