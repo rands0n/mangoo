500d4d0017c1884e5666e3e59618134cdc5c4319
Randson Oliveira <orandson@gmail.com>
2016-10-12 17:43:58 -0300

    refactor(lang): add isFunction to global export

--- src/lang.js
+++ src/lang.js
@@ -1,10 +1,12 @@
 import { isArray } from './lang/isArray'
+import { isFunction } from './lang/isFunction'
 import { isNull } from './lang/isNull'
 import { isObject } from './lang/isObject.js'
 import { toString } from './lang/toString'
 
 export {
   isArray,
+  isFunction,
   isNull,
   isObject,
   toString
