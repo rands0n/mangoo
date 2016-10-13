691f5cae44e2b6090c6e9d73e773cbb25d8ef478
Randson Oliveira <orandson@gmail.com>
2016-10-12 23:25:24 -0300

    refactor(array): change order of arguments in filter and apply some to function

--- tests/array/filter.spec.js
+++ tests/array/filter.spec.js
@@ -5,14 +5,14 @@ describe('Array', () => {
     context('when passing numbers', () => {
       it('reduces an array to those matching a filter', () => {
         let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        let result = filter((item) => item % 2 === 0, arr)
+        let result = filter(arr, (item) => item % 2 === 0)
 
         expect(result).to.eql([2, 4, 6, 8, 10])
       })
 
       it('reduces an array to those numbers are greater than 100', () => {
         let arr = [1, 100, 200, 300, 201]
-        let result = filter((i) => i >= 100, arr)
+        let result = filter(arr, (i) => i >= 100)
 
         expect(result).to.eql([100, 200, 300, 201])
       })
@@ -20,7 +20,7 @@ describe('Array', () => {
       context('when passing negative numbers', () => {
         it('reduces an array to those numbers are negative', () => {
           let arr = [-1, -100, -200, 0, 300]
-          let result = filter((i) => i < 0, arr)
+          let result = filter(arr, (i) => i < 0)
 
           expect(result).to.eql([-1, -100, -200])
         })
@@ -30,19 +30,19 @@ describe('Array', () => {
     context('when passing wrong arguments', () => {
       it('reduces to an empty array if its empty', () => {
         let arr = []
-        let result = filter((i) => i > 0, arr)
+        let result = filter(arr, (i) => i > 0)
 
         expect(result).to.eql([])
       })
 
       it('reduces to an empty array if value is undefined', () => {
-        let result = filter((i) => i > 0, undefined)
+        let result = filter(undefined, (i) => i > 0)
 
         expect(result).to.eql([])
       })
 
       it('reduces to an empty array if value is void', () => {
-        let result = filter((i) => i > 0, void 0)
+        let result = filter(void 0, (i) => i > 0)
 
         expect(result).to.eql([])
       })
